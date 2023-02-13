e4bdc49853f3545712a2b98af86d5411076a28b0
ArachDyrr <1021548@hr.nl>
2023-02-13 13:38:12 +0100

    werk ochtend 13-2

---
+++ practica/week2/dag06/dict workfile.py
@@ -0,0 +1,4 @@
+marslanderSpecs = { 'length': 6, 'width': 1.56, 'weight': 360, 'deckHeight': (83, 108), 'robotArmLength': 1.8, 'numberOfSolarPanels': 2}
+marslanderSpecs.items()
+
+print (marslanderSpecs)
